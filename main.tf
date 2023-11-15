4d55ccadf4e31e80231523e5ae654ce481b339b2
mahesh1b <mahesh22071999@gmail.com>
2023-11-15 11:04:15 +0530

    added the main.tf and gitignore file

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "playground"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "webapp" {
+  ami           = "AMI ID"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "Webapp"
+  }
+}
